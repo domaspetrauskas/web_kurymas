6d0f996be7452a1bcea75ae017d0228fc38600db
domaspetrauskas <domas.petrauskas2@gmail.com>
2023-07-27 15:36:51 +0300

    LLLLL

--- domas_baigiamasis/domas_baigiamasis.php
+++ domas_baigiamasis/domas_baigiamasis.php
@@ -8,11 +8,11 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link rel="stylesheet" href="css/domas_baigiamasis.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="js/domas_baigiamasis.js">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="js/domas_baigiamasis.js"></script>
 </head>
 
 <body>
